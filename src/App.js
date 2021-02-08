71fb65f13c48cf2b458fb40416fe2fd1452cd661
JaksonSouza <jakson.rts@hotmail.com>
2021-02-08 00:07:58 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      showImage: false,
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      showImage: !this.state.showImage,
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button type="button" onClick={this.toggle}>
+            {this.state.showImage ? 'Esconder' : 'Monstrar'}
+          </button>
+          {this.state.showImage &&(
+            <img src={logo} className="App-logo" alt="logo" />
+          )}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
